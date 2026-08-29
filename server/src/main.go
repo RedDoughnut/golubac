92129491d1f681bc9273a8e3c35e71f2ec393dab
David Tomic <davidtomic1010@gmail.com>
2026-08-29 20:52:58 +0200

    Added register(incomplete) and hashing

--- server/src/main.go
+++ server/src/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"net/http"
-	//"os"
-	//"context"
+	//"net/http"
+	"context"
 	"fmt"
-	//"github.com/jackc/pgx/v5"
+	"os"
+
+	"src/src/auth"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+
 	//"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type SendMessageRequest struct {
@@ -18,10 +24,28 @@ type SendMessageRequest struct {
 type LoginRequest struct {
 	Username string `json:"username"`
 }
-
-func helloworld(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "Hello World!")
+type Server struct {
+	DB pgx.Conn
 }
+type RegisterRequest struct {
+	Email       string `json:"email"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	DisplayName string `json:"displayname"`
+}
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
+
 func Login(c *gin.Context) {
 	var input LoginRequest
 
@@ -36,7 +60,33 @@ func Login(c *gin.Context) {
 
 	c.JSON(200, gin.H{"token": "MGnajjacaSkola." + input.Username})
 }
-func SendMessage(c *gin.Context) {
+
+func (s *Server) Register(c *gin.Context) {
+	var input RegisterRequest
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	if input.Email == "" || input.DisplayName == "" || input.Username == "" || input.Password == "" {
+		c.JSON(400, gin.H{"error": "Missing fields."})
+		return
+	}
+	salt, err := auth.GenerateSalt()
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	hashed_pass := auth.HashPassword(input.Password, salt)
+	_,err = s.DB.Exec(context.Background(), "INSERT INTO users (username, display_name, email, hashed_password, salt) VALUES ($1,$2,$3,$4,$5);", input.Username, input.DisplayName, input.Email, hashed_pass, salt)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{"refresh"})
+}
+func SendTextMessage(c *gin.Context) {
 	var data SendMessageRequest
 	if err := c.ShouldBindJSON(&data); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
@@ -49,17 +99,29 @@ func SendMessage(c *gin.Context) {
 	c.Status(200)
 	fmt.Printf("Got a successful request! token: %s\nrecepient: %s\nmessage: %s\n", data.Token, data.Recipient, data.Message)
 }
+func printError(format string, a ...interface{}) {
+	fmt.Printf("\033[1;31m"+format+"\033[0m\n", a...)
+}
+
+// Loads enviromental variables
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		printError("Error loading .env file")
+	}
+}
 func main() {
-	// ctx := context.Background()
-	// targetdb := "golubac"
-	// username := "admin"
-	// password := "admin"
-	// host := "localhost"
-	// port := "18000"
+	loadEnv()
+	PostgresURL := os.Getenv("POSTGRES_URL")
+	conn, err := pgx.Connect(context.Background(), PostgresURL)
+	if err != nil {
+		printError("Error connecting to DB: %s", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
 
 	router := gin.Default()
-	router.GET("/helloworld", helloworld)
 	router.POST("/login", Login)
-	router.POST("/send-message", SendMessage)
+	router.POST("/send-text-message", SendMessage)
 	router.Run("0.0.0.0:18000")
 }
